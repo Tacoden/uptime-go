b1244cff23db203eb6ea1917efa868550abe779b
Dennis.G <52651867+DennisGrellmann@users.noreply.github.com>
2026-03-26 18:43:03 +1100

    Add configuration for time and update main logic for improved clarity and added interval based ping

--- main.go
+++ main.go
@@ -4,15 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-
+	"time"
 	"github.com/go-ping/ping"
 )
 
 type Config struct {
-	IPAddresses []string `json:"target_ip_addresses"`
+	IPAddresses  []string `json:"target_ip_addresses"`
+	IntervalSecs int      `json:"interval_seconds"`
 }
 
-func main() {
+func ReadConfig() Config {
 	file, err := os.ReadFile("config.json")
 	if err != nil {
 		panic(err)
@@ -23,7 +24,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	return config
 
+}
+
+func Ping(config Config) {
 	for i, ip := range config.IPAddresses {
 		fmt.Printf("IP %d: %s\n", i+1, ip)
 
@@ -37,4 +42,25 @@ func main() {
 		stats := pinger.Statistics()
 		fmt.Printf("%+v\n", stats)
 	}
+}
+
+func ReadTime(config Config) time.Duration {
+	return time.Duration(config.IntervalSecs) * time.Second
+}
+
+func Timer(sec time.Duration, config Config) {
+	ticker := time.NewTicker(sec)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		Ping(config)
+	}
+}
+
+func main() {
+
+	config := ReadConfig()
+	sec := ReadTime(config)
+	Ping(config)
+	Timer(sec, config)
 }
